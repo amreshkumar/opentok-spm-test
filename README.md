3b5227806961e9608961b95aed744e8325e3b5fc
Amresh Kumar <amresh.kumar@care.com>
2022-01-07 11:39:16 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ios-spm-opentok
+
+A description of this package.
---
+++ Sources/ios-spm-opentok/ios_spm_opentok.swift
@@ -0,0 +1,6 @@
+public struct ios_spm_opentok {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
